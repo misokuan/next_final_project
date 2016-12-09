1aec8a5f1088befe7893baa056621a2a21042397
Jeffrey Goh Wei Xiang <jgohwx@example.com>
2016-12-09 18:38:15 +0800

    Added keys to image uploader for s3

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,17 @@
+config.fog_provider = 'fog' # 'fog/aws' etc. Defaults to 'fog'
+config.fog_credentials = { ... } # Provider specific credentials
+
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     ENV[AWS_ACCESS_KEY],                        # required
+    aws_secret_access_key: ENV[AWS_SECRET_ACCESS_KEY],                        # required
+    region:                'us-west-2',                  # optional, defaults to 'us-east-1'
+    host:                  's3.example.com',             # optional, defaults to nil
+    endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+  }
+  config.fog_directory  = 'avatarimage'                          # required
+  config.fog_public     = false                                        # optional, defaults to true
+  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+end
