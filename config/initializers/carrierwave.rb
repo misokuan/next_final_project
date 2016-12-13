7a10e1e4d4dc9a31aaef9d886b59d4432cc45a20
Jeffrey Goh Wei Xiang <jgohwx@example.com>
2016-12-13 13:39:43 +0800

    Switched fog to file mode for storing images

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -18,11 +18,14 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV['AWS_ACCESS_KEY'],                        # required
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],                        # required
-    region:                'us-west-2'                  # optional, defaults to 'us-east-1'
+    # region:                'us-west-2',                  # optional, defaults to 'us-east-1'
+    # path_style:            true,
+    # host:                  'https://bucketname.s3.amazonaws.com',             # optional, defaults to nil
     # host:                  's3.example.com',             # optional, defaults to nil
     # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
-  config.fog_directory  = 'avatarimage'                          # required
+  
+  config.fog_directory  = ENV['AWS_BUCKET']                          # required
   # config.fog_public     = false                                        # optional, defaults to true
   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
