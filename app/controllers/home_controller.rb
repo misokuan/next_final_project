614d5fce199342de4ae150bf1f3dfb525db3132e
Kuan <pui_kuan93@hotmail.com>
2016-12-14 15:40:44 +0800

    fixed landing page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,13 @@
 class HomeController < ApplicationController
 	def index
-		if signed_in? 
-			redirect_to user_campaign_path(current_user, current_user.campaign.id) if current_user.campaign != nil
-			redirect_to user_profile_path(current_user, current_user.profile.id) if current_user.profile != nil
+		if signed_in?
+			if current_user.campaign != nil
+				redirect_to user_campaign_path(current_user, current_user.campaign.id)
+			elsif current_user.profile != nil
+				redirect_to user_profile_path(current_user, current_user.profile.id)
+			else
+				redirect_to new_user_profiles_path
+			end
 		end
 	end 
 end
