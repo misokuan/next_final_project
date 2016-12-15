abcce0aa47be4fbbcea5cc3c3687b7831dc17cda
Kuan <pui_kuan93@hotmail.com>
2016-12-15 21:20:41 +0800

    assigned diff rewards_class to diff amount

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -11,10 +11,20 @@ class RewardsController < ApplicationController
   end
 
   def create
+    num = 1
   	params["rewards"].each do |r|
   		if r["amount"] != "" || r["description"] != ""
   			new_reward = Reward.create(campaign_id: params[:campaign_id])
+        case num
+          when 1
+            new_reward.update(rewards_class: "alpha")
+          when 2
+            new_reward.update(rewards_class: "beta")
+          when 3
+            new_reward.update(rewards_class: "gamma")
+        end
         new_reward.update(reward_params(r))
+        num += 1
   		end
   	end
   	redirect_to user_campaign_path(user_id: current_user.id, id: Reward.last.campaign.id)
