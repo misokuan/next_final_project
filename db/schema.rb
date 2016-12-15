6cbd81db0c4e030dbaf901257f9f0812a9117ab2
Kuan <pui_kuan93@hotmail.com>
2016-12-15 15:11:59 +0800

    fixed foreign key violation problem

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20161213042329) do
-
-
+ActiveRecord::Schema.define(version: 20161215070952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,7 +116,6 @@ ActiveRecord::Schema.define(version: 20161213042329) do
     t.string   "encrypted_password",    limit: 128
     t.string   "confirmation_token",    limit: 128
     t.string   "remember_token",        limit: 128
-    t.string   "avatar"
     t.string   "password_confirmation"
   end
 
@@ -127,5 +123,4 @@ ActiveRecord::Schema.define(version: 20161213042329) do
   add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
   add_foreign_key "comments", "posts"
-  add_foreign_key "posts", "users", column: "campaign_id"
 end
