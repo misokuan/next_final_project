bd36989e023f677f012af945de8d0167496c5996
Jeffrey Goh Wei Xiang <jgohwx@example.com>
2016-12-15 15:17:26 +0800

    Tweaked Image uploaders to include multiple version files and worked multi-image uploader with jpg

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -23,7 +23,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process resize_to_fill: [800, 1200]
+  process resize_to_fit: [800, 800]
   #
   # def scale(width, height)
   #   # do something
@@ -31,7 +31,7 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [150, 100]
+    process resize_to_fit: [300, 300]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
