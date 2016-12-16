f6ab0ed53536f19921459287922861b74eea89bd
ejaytkm@gmail.com <ejaytkm@gmail.com>
2016-12-16 10:44:01 +0800

    added payment table which produces a receipt

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161215080052) do
+ActiveRecord::Schema.define(version: 20161216022809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,18 @@ ActiveRecord::Schema.define(version: 20161215080052) do
   end
 
   add_index "interests", ["category_id"], name: "index_interests_on_category_id", using: :btree
+
+  create_table "payments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "campaign_id"
+    t.integer  "transaction_amount"
+    t.string   "transaction_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
+
+  add_index "payments", ["campaign_id"], name: "index_payments_on_campaign_id", using: :btree
+  add_index "payments", ["user_id"], name: "index_payments_on_user_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.integer  "campaign_id"
