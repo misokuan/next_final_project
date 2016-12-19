0814616787ef843513e4bb1cd27ba35bf2c448ed
Kuan <pui_kuan93@hotmail.com>
2016-12-20 00:53:52 +0800

    profile #edit, profile #show, navbar avatar, profile-interest linkage

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,5 +2,14 @@ class Profile < ActiveRecord::Base
 	has_many :profile_interests
 	has_many :interests, :through => :profile_interests
 	belongs_to :user
+	acts_as_birthday :date_of_birth
 	mount_uploader :avatar, AvatarUploader
+
+	def full_name
+	  if self.first_name != nil || self.last_name != nil
+		return "#{self.first_name.titleize} #{self.last_name.titleize}"
+	  else
+	  	return "N/A"
+	  end
+	end
 end
